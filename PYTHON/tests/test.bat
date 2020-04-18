608a1772efeb294f9758f834366280b80a79aaa4
TreasureMaster <54545559+TreasureMaster@users.noreply.github.com>
2020-04-18 10:45:35 +0300

    add bat-files

--- PYTHON/tests/test.bat
+++ PYTHON/tests/test.bat
@@ -1,1 +1,8 @@
-pytest -v test_calc_alu_add.py
+@echo off
+
+if "%1" == "" help.bat
+if %1 == add pytest -v test_calc_alu_add.py
+if %1 == flags pytest -v test_flags.py
+if %1 == int pytest -v test_calculator.py
+if %1 == float pytest -v test_calc_float.py
+if %1 == help help.bat
